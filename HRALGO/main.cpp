44b56863b938d80b19b9fce0cf75f90ec7f5fd93
Rishabh Purwar <rishabh.purwar@Rishabhs-MacBook-Air.local>
2016-05-05 16:29:30 +0530

    Initial Commit

---
+++ HRALGO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HRALGO
+//
+//  Created by Rishabh Purwar on 05/05/16.
+//  Copyright © 2016 Rustyman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
